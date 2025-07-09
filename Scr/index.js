e091235380fb7a5a6cf6bc1d43621ed9a61a89ed
moohhiit <itzmohitsharma20@gmail.com>
2025-07-09 20:01:32 +0530

    Adding Room Create Socke

--- Scr/index.js
+++ Scr/index.js
@@ -28,9 +28,12 @@ const io = new Server(server, {
 app.use(cors())
 
 let connectedUsers = []
+let roomList = []
+
+// Socket Server
 io.on('connection', (socket) => {
   console.log('client connected:', socket.id);
-
+  io.emit("update-room", roomList);
 
   socket.on("register-user", (username) => {
     let userdata = { id: socket.id, username: username }
@@ -39,7 +42,6 @@ io.on('connection', (socket) => {
   });
 
   socket.on('private_message', ({ from, to, message }) => {
-    console.log("Message Recive" , from , to , message)
 
     if (to) {
       io.to(to).emit('private_message', {
@@ -62,6 +64,22 @@ io.on('connection', (socket) => {
     }
   });
 
+  // Room Create 
+  socket.on("room_create", ({ room_name, creater_id }) => {
+    let room_data = { room_name, creater_id }
+    roomList.push(room_data)
+    io.emit("update-room", roomList);
+    socket.join(room_name)
+  })
+
+  socket.on("room_message", ({ room_name, user_name, message }) => {
+    socket.on(room_name).emit("room_message", {
+      room_name, user_name, message
+    })
+  })
+
+
+
   socket.on('disconnect', () => {
     connectedUsers = connectedUsers.filter(user => user.id !== socket.id)
     io.emit("update-users", connectedUsers);
@@ -71,14 +89,14 @@ io.on('connection', (socket) => {
 
 app.use(express.json())
 
-app.get('/' , (req  ,res)=>{
-  res.send({"status" : "Server Running"})
+app.get('/', (req, res) => {
+  res.send({ "status": "Server Running" })
 })
 
 
 app.use('/api/auth', AuthRoute)
 app.use('/api/chat', ChatRoute)
-app.use("/ai" , GenAI)
+app.use("/ai", GenAI)
 
 
 
